a2e2a519f54f9f11b52b5b1073a62af96e37ba0e
Iskander <iskandar.13.aburahmonov@gmail.com>
2025-06-19 13:26:56 +0500

    implemented based responsiveness and voice recognition

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import {useRouter} from "vue-router";
 
+const router = useRouter()
+
+const gotoKanban = () => {
+	router.push({ name: 'Home'})
+}
 </script>
 
 <template>
 	<h1 class="font-bold mt-10">Task Board</h1>
+	<n-button @click="gotoKanban">Go to Kanban</n-button>
 </template>
 
 <style scoped>
